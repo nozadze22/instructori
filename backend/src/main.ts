3da2d9cd5abe72fb9fdf00cb50f069bb347a60c9
nozadze22 <giorgin432@gmail.com>
2026-08-23 14:47:40 +0400

    deploy errors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -31,7 +31,9 @@ async function bootstrap() {
     credentials: true,
   });
 
-  const port = Number(process.env.PORT ?? 4000);
+  const port = process.env.FLY_APP_NAME
+    ? 8080
+    : Number(process.env.PORT ?? 4000);
   await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
